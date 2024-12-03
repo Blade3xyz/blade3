03e1cf05a248206a2bdb32b688a8344b61de52b9
interfiberschool <117294755+interfiberschool@users.noreply.github.com>
2024-12-03 17:45:46 +0000

    Blade3 crypto wrapper

---
+++ crypto.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+require "openssl"
+require "logger"
+require "base64"
+
+class Crypto
+    attr_accessor :key
+    attr_accessor :encrypted
+    attr_accessor :cipher
+    attr_accessor :key
+    
+    def initialize(encrypted = true)
+        @encrypted = encrypted
+
+        @logger = Logger.new(STDOUT)
+
+        @logger.info "Initalizing encryption using AES-256-CBC"
+
+        @cipher = OpenSSL::Cipher.new("aes-256-cbc")
+        if encrypted
+            @cipher.encrypt
+        else
+            @cipher.decrypt
+        end
+
+        if not File.exist?("blade3.key")
+            @key = @cipher.random_key
+
+            File.binwrite("blade3.key", @key)
+
+            @logger.debug "Wrote blade3 key to: ./blade3.key"
+        else
+            @key = File.binread("blade3.key")
+
+            @logger.debug "Imported key from ./blade3.key"
+        end
+
+        @cipher.key = @key
+
+        @cipher.iv = "0"*16
+    end
+
+    def encrypt(message)
+        if not @encrypted
+            raise "Encryption disabled for this Crypto instance!"
+        end
+        final = Base64::encode64(@cipher.update(message) + @cipher.final)
+
+        @cipher.reset
+
+        return final
+    end
+
+    def decrypt(message)
+        if @encrypted
+            raise "Encryption enabled for this Crypto instance!"
+        end
+
+        final = @cipher.update(Base64::decode64(message)) + @cipher.final
+
+        @cipher.reset
+
+        return final
+    end
+end
+
+cryp = Crypto.new()
+decrypt = Crypto.new(false)
+encrypted = cryp.encrypt("Hello World 123")
+puts decrypt.decrypt(encrypted)
